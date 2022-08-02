c1edeb90079aaeb5a7c2e4a3869d8e4801814e1c
sofia <sofiainesschenone@gmail.com>
2022-08-02 15:09:55 -0300

    site.xml y txt

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDwyVoMIUDPl_I_Fqj_ktll7OgymjWKz7w',
+    authDomain: 'domselaarcomerciosyservicios.firebaseapp.com',
+    projectId: 'domselaarcomerciosyservicios',
+    storageBucket: 'domselaarcomerciosyservicios.appspot.com',
+    messagingSenderId: '368224801430',
+    appId: '1:368224801430:web:4993f32e55cfcc6832a8b7',
+    measurementId: 'G-SBD4Z65DEG',
+  },
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDwyVoMIUDPl_I_Fqj_ktll7OgymjWKz7w',
+    authDomain: 'domselaarcomerciosyservicios.firebaseapp.com',
+    projectId: 'domselaarcomerciosyservicios',
+    storageBucket: 'domselaarcomerciosyservicios.appspot.com',
+    messagingSenderId: '368224801430',
+    appId: '1:368224801430:web:4993f32e55cfcc6832a8b7',
+    measurementId: 'G-SBD4Z65DEG',
+  },
 };
 
 /*
