556f8e8a5c37e51ea2cc7be341b0271ec8eba911
sofia <sofiainesschenone@gmail.com>
2022-10-19 16:14:11 -0300

    se agregó un remís

--- src/app/comercios/comercios.component.ts
+++ src/app/comercios/comercios.component.ts
@@ -10,12 +10,13 @@ export class ComerciosComponent implements OnInit {
   comercios: any[] =[
     {nombreComercio:"Remís",
     datosComercio:[
-    { nameComer: 'Remís Domselaar', tel:"112224442977", telComer: 'https://wa.me/5492224442977' },
-    { nameComer: 'Remís Seba Perro', tel:"+ 54 92224 54 7612", telComer: 'https://wa.me/5492224547612' },
-    { nameComer: 'Remis Ds (Carlos)', tel:"+54 91167 924079", telComer: 'https://wa.me/5491167924079'},
-    { nameComer: 'Marcos REMIS DOMSE', tel:"+54 9 11 4145-6515", telComer: 'https://wa.me/5491141456515' },
-    { nameComer: 'Remis Luis', tel:"+54 9 11 5146-7452", telComer: 'https://wa.me/5491151467452' },
-    { nameComer: 'Remis Sebastián', tel:"+54 9 11 3765-4387", telComer: 'https://wa.me/5491137654387' }
+    { nameComer: 'Remís Domselaar', tel:"222 444 2977", telComer: 'https://wa.me/5492224442977' },
+    { nameComer: 'Remís Seba Perro', tel:"2224 54 7612", telComer: 'https://wa.me/5492224547612' },
+    { nameComer: 'Remis Ds (Carlos)', tel:"1167 924079", telComer: 'https://wa.me/5491167924079'},
+    { nameComer: 'Marcos REMIS DOMSE', tel:"11 4145-6515", telComer: 'https://wa.me/5491141456515' },
+    { nameComer: 'Remis Luis', tel:"11 5146-7452", telComer: 'https://wa.me/5491151467452' },
+    { nameComer: 'Remis Sebastián', tel:"11 3765-4387", telComer: 'https://wa.me/5491137654387' },
+    { nameComer: 'Remis Nelly', tel:"11 6008 0701 - 11 5017 4195", telComer: 'https://wa.me/5491160080701' }
   ]},
 
     {nombreComercio:"Almacenes y Supermercados", datosComercio:
